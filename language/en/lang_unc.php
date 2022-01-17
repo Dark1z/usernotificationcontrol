558f3a93246590f6e4073ef509f1a88db56f7502
Dark❶ <25451052+Dark1z@users.noreply.github.com>
2022-01-17 23:08:22 +0530

    UNC v1.1.1
    - phpBB CDB changes
    - CopyRight Year to ForEver
    
    Signed-off-by: Dark❶ <25451052+Dark1z@users.noreply.github.com>

--- language/en/lang_unc.php
+++ language/en/lang_unc.php
@@ -3,7 +3,7 @@
  *
  * User Notification Control [UNC]. An extension for the phpBB Forum Software package.
  *
- * @copyright (c) 2020-2021, Dark❶, https://dark1.tech
+ * @copyright (c) 2020-forever, Dark❶, https://dark1.tech
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
  *
